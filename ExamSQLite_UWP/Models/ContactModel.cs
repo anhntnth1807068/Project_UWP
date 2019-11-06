7e043f2bb39622dc16d70710b0bc28699773c0e1
Anh Hoc Dot <anh@hocdot.com>
2019-11-06 16:05:04 +0700

    baithiUWP

---
+++ ExamSQLite_UWP/Models/ContactModel.cs
@@ -0,0 +1,84 @@
+﻿using ExamSQLite_UWP.Entity;
+using ExamSQLite_UWP.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSQLite_UWP.Models
+{
+    class ContactModel
+    {
+        public bool Insert(Contact contact)
+        {
+            try
+            {
+                using (var stt = SQLiteUtil.GetIntances().Connection.Prepare("INSERT INTO Note (Name, PhoneNumber) VALUES ( ? , ?)"))
+                {
+                    stt.Bind(1, contact.Name);
+                    stt.Bind(2, contact.PhoneNumber);
+                    
+                    stt.Step();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return false;
+        }
+
+        public List<Contact> Select()
+        {
+            try
+            {
+                List<Contact> lstNote = new List<Contact>();
+                using (var stt = SQLiteUtil.GetIntances().Connection.Prepare("SELECT * from Note"))
+                {
+                    while (stt.Step() == SQLitePCL.SQLiteResult.ROW)
+                    {
+                        var getContact = new Contact();
+                        getContact.Id = Convert.ToInt32(stt[0]);
+                        getContact.Name = (string)stt[1];
+                        getContact.PhoneNumber = (string)stt[2];
+                        lstNote.Add(getContact);
+                    }
+                    return lstNote;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        //public Contact SelectOneItem(int? id)
+        //{
+        //    try
+        //    {
+        //        var getContact = new Contact();
+        //        using (var stt = SQLiteUtil.GetIntances().Connection.Prepare("SELECT * from Note Where Id = ?"))
+        //        {
+        //            stt.Bind(1, id);
+        //            if (stt.Step() == SQLitePCL.SQLiteResult.ROW)
+        //            {
+        //                getContact.Name = (string)stt[1];
+        //                getContact.PhoneNumber = (string)stt[2];
+        //            }
+        //            return getContact;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Debug.WriteLine(ex.Message);
+        //        return null;
+        //    }
+        //}
+
+        }
+    }
+
---
+++ ExamSQLite_UWP/Utils/SQLiteUtil.cs
@@ -0,0 +1,46 @@
+﻿using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSQLite_UWP.Utils
+{
+    class SQLiteUtil
+    {
+        private const string DatabaseName = "baithi.db";
+
+        private static SQLiteUtil _instance;
+        public SQLiteConnection Connection { get; }
+
+        public static SQLiteUtil GetIntances()
+        {
+            if (_instance == null)
+            {
+                _instance = new SQLiteUtil();
+            }
+            return _instance;
+        }
+
+        private SQLiteUtil()
+        {
+            Connection = new SQLiteConnection(DatabaseName);
+            CreateTables();
+        }
+
+        private void CreateTables()
+        {
+            string sql = @"CREATE TABLE IF NOT EXISTS AddContact (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Name VARCHAR( 140 ),PhoneNumber VARCHAR( 140 ));";
+            using (var statement = Connection.Prepare(sql))
+            {
+                statement.Step();
+            }
+            string SQL = @"CREATE TABLE IF NOT EXISTS SearchContact (Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,Name VARCHAR( 140 ),PhoneNumber VARCHAR( 140 ));";
+            using (var statement = Connection.Prepare(SQL))
+            {
+                statement.Step();
+            }
+        }
+    }
+}
